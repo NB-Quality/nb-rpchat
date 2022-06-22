83d9f3a4ffd1f0bf9e15de031da9e388cafbf630
negbook <negbook@gmail.com>
2022-06-22 23:02:04 +0800

    new

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+Config.PlayTagSound = true 
+Config.Locales = {
+    ['dice'] = "%s Throws a Dice that lands on %d",
+    ['ooc'] = "(( %s: %s ))",
+    ['me'] = "* %s %s",
+    ['close'] = "* %s Says: %s",
+    ['local'] = "* %s Says: %s",
+    ['b'] = "* %s Says: (( %s ))",
+    ['shout'] = "* %s Shouts: %s",
+}
